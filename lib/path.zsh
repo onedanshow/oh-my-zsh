5c16945cde4ee52089ac2f7ee19c3ba6c6089a27
Daniel Dixon <code@danieldixon.com>
2016-06-24 10:49:58 -0500

    Added Shopify's ThemeKit

--- lib/path.zsh
+++ lib/path.zsh
@@ -1,2 +1,5 @@
 # DD: added, so that Titanium Appcelerator will work: http://developer.appcelerator.com/question/145794/launching-simulator-process-failed
 export PATH=$PATH:/usr/local/share/npm/bin
+
+# DD: for Shopify's Theme Kit
+export PATH=/Users/daniel/.themekit:$PATH
